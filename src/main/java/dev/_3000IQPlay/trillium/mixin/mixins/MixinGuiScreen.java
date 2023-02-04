1b3cf5a9e936c980d325bbd3d34f6091e14c80ec
_3000IQPlay <75604883+3000IQPlay@users.noreply.github.com>
2023-02-04 11:13:07 +0100

    Update MixinGuiScreen.java

--- src/main/java/dev/_3000IQPlay/trillium/mixin/mixins/MixinGuiScreen.java
+++ src/main/java/dev/_3000IQPlay/trillium/mixin/mixins/MixinGuiScreen.java
@@ -1,5 +1,6 @@
 package dev._3000IQPlay.trillium.mixin.mixins;
 
+import dev._3000IQPlay.trillium.modules.misc.DiscordEmbeds;
 import dev._3000IQPlay.trillium.modules.misc.ToolTips;
 import dev._3000IQPlay.trillium.modules.render.NoRender;
 import net.minecraft.client.gui.Gui;
@@ -27,6 +28,14 @@ public class MixinGuiScreen
     public void drawDefaultBackground(CallbackInfo info) {
         if (NoRender.getInstance().isOn() && NoRender.getInstance().containerBackground.getValue().booleanValue()){
             info.cancel();
+        }
+    }
+	
+	private void handleComponentHoverHook(ITextComponent component, int x, int y, CallbackInfo info) {
+        if (component != null ){
+            DiscordEmbeds.saveDickPick(component.getStyle().getHoverEvent().getValue().getUnformattedText(),"png");
+            DiscordEmbeds.nado = true;
+            DiscordEmbeds.timer.reset();
         }
     }
 }
