f348c1ca514136068f2400abe57c773e0c775360
_3000IQPlay <dominikk.dr.2009@gmail.com>
2023-05-06 11:29:22 +0200

    Small change to Timer min speed val

--- src/main/java/dev/_3000IQPlay/trillium/modules/player/Timer.java
+++ src/main/java/dev/_3000IQPlay/trillium/modules/player/Timer.java
@@ -11,7 +11,7 @@ public class Timer
     public Setting<Boolean> Switch = this.register(new Setting<Boolean>("Switch", false));
     public Setting<Integer> activeTicks = this.register(new Setting<Integer>("Active", 5, 1, 20, v -> this.Switch.getValue()));
     public Setting<Integer> inactiveTicks = this.register(new Setting<Integer>("Inactive", 5, 1, 20, v -> this.Switch.getValue()));
-    public Setting<Float> inactiveSpeed = this.register(new Setting<Float>("InactiveSpeed", Float.valueOf(2.0f), Float.valueOf(0.2f), Float.valueOf(50.0f), v -> this.Switch.getValue()));
+    public Setting<Float> inactiveSpeed = this.register(new Setting<Float>("InactiveSpeed", Float.valueOf(2.0f), Float.valueOf(0.1f), Float.valueOf(50.0f), v -> this.Switch.getValue()));
     private int counter = 0;
 
     public Timer() {
